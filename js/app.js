f725a8a715b2a08ef4514c0dd09b8fe3b7ed7efb
piyushkarmhe <karmhepiyush@gmail.com>
2023-08-11 15:16:50 +0530

    smooth scroll

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ loadingElement.style.display = "none";
 
 AOS.init({
   duration: 1200,
-})  //AOS animations
+}); //AOS animations
 
 // Loader
 // window.addEventListener("load", function () {
@@ -65,3 +65,50 @@ if (window.matchMedia("(min-width: 768px)").matches) {
   });
 }
 // Astronaut Scroll Animation End
+
+//Smooth Scroll
+let smoothscrollLinks = document.querySelectorAll(".smoothscroll");
+
+for (let i = 0; i < smoothscrollLinks.length; i++) {
+  
+  smoothscrollLinks[i].addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let target = document.querySelector(this.getAttribute("href"));
+
+    if (target) {
+      let startPosition = window.pageYOffset;
+      let targetPosition = target.getBoundingClientRect().top-100;
+      // let targetPosition = $target.getBoundingClientRect().top + startPosition;
+
+      let startTime = null;
+
+      function scrollAnimation(timestamp) {
+        if (!startTime) startTime = timestamp;
+        let progress = timestamp - startTime;
+        let duration = 1000; // Animation duration in milliseconds
+
+        window.scrollTo(
+          0,
+          easeInOutCubic(
+            progress,
+            startPosition,
+            targetPosition - startPosition,
+            duration
+          )
+        );
+
+        if (progress < duration) {
+          requestAnimationFrame(scrollAnimation);
+        }
+      }
+
+      requestAnimationFrame(scrollAnimation);
+    }
+  });
+}
+
+function easeInOutCubic(t, b, c, d) {
+  if ((t /= d / 2) < 1) return (c / 2) * t * t * t + b;
+  return (c / 2) * ((t - 2) * t * t + 2) + b;
+}
