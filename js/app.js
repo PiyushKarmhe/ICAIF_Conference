782d9774d0d206a28894c702c2efe9e07823bcb2
piyushkarmhe <karmhepiyush@gmail.com>
2023-08-11 13:55:38 +0530

    intermediate

--- js/app.js
+++ js/app.js
@@ -8,28 +8,31 @@ function sigmoid(x) {
 
 if (window.matchMedia("(min-width: 768px)").matches) {
   window.addEventListener("scroll", () => {
+    
     const scrollY = window.scrollY;
     const maxScroll = window.innerHeight;
-
-    console.log("Scroll : ",scrollY," maxscroll : ",maxScroll);
 
     let newRight =
       initialRight * (initialRight / (initialRight + scrollY * 10));
     const newScale = initialRight / (initialRight + scrollY * 10);
 
     if(scrollY >= maxScroll){
-      const opacity = sigmoid((maxScroll-scrollY)*0.01);
+      let opacity = sigmoid((maxScroll-scrollY)*0.01);
+      if(opacity < 0.01) opacity = 0;
       scrollElement.style.opacity = `${opacity}`;
       console.log("opacity : ",opacity);
     }else{
+
       const opacity = sigmoid((maxScroll-scrollY)*0.01);
       scrollElement.style.opacity = `${opacity}`;
+
       if (newRight > 0 && newRight <= 25) {
         newRight -= 5;
         scrollElement.style.right = `${newRight}px`;
       } else if (newRight >= 0) {
         scrollElement.style.right = `${newRight}px`;
       }
+
       if (newScale >= 0.4) {
         scrollElement.style.transform = `scale(${newScale})`;
         scrollElement.style.transform += `transform: rotate(-15deg)`;
